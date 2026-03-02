d11247b97e03583bc8397f3fe0b3c9a752bcd8ac
saleem <ab588538@gmail.com>
2026-03-02 16:42:31 +0530

    add the navbar and setup all pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Navbar from "./components/layout/Navbar";
+import Service from "./pages/Service";  
+import Project from "./pages/Project";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
@@ -10,6 +15,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
